f8fd62b6e76bb94baac1bc39fd9cd18e41d9f1f9
rodukao <rodukao@gmail.com>
2026-04-28 10:13:40 -0300

    feat: acrescentado funcoes para inserir, validar, atualizar e deletar usuários

--- backend/app.py
+++ backend/app.py
@@ -24,12 +24,13 @@ class Usuarios:
     def insert_user(self, nome, email, senha):
         salt = bcrypt.gensalt()
         senha_hashed = bcrypt.hashpw(senha, salt)
-        parametros = (nome, email, senha_hashed, datetime.now())
+        parametros = (nome, email, senha_hashed.decode(
+            "utf-8"), datetime.now())
         try:
             self.cursor.execute(
                 f"""INSERT INTO usuarios (nome, email, senha, createdAt) VALUES (%s, %s, %s, %s);""", parametros)
             self.connection.commit()
-            return "Sucesso. Usuário inserido com sucesso"
+            return "Sucesso. Usuário inserido com sucesso."
         except Exception as e:
             return f"Erro. Falha ao inserir usuário: {e}"
 
@@ -42,7 +43,37 @@ class Usuarios:
         except Exception as e:
             return f"Erro. Fala ao buscar usuário: {e}"
 
+    def validate_user(self, email, senha):
+        try:
+            self.cursor.execute(
+                """SELECT * FROM usuarios WHERE email = %s""", (email,))
+            usuario = self.cursor.fetchone()
+            senha_banco = usuario[3].encode("utf-8")
+            valid_user = bcrypt.checkpw(senha.encode("utf-8"), senha_banco)
+            if valid_user:
+                return usuario
+            else:
+                return f"Erro. Email ou senha incorretos."
+        except Exception as e:
+            return f"Erro. Falha ao validar usuário: {e}"
 
-gestor_usuarios = Usuarios()
-# print(gestor_usuarios.insert_user("Rodrigo Duque", "rodukao@gmail.com", b"123456"))
-print(gestor_usuarios.return_user("rodukao@gmail.com"))
+    def atualiza_senha(self, email, nova_senha):
+        salt = bcrypt.gensalt()
+        nova_senha_hashed = bcrypt.hashpw(nova_senha, salt)
+        parametros = (nova_senha_hashed, email)
+        try:
+            self.cursor.execute(
+                """UPDATE usuarios SET senha = %s WHERE email = %s""", (parametros))
+            self.connection.commit()
+            return "Sucesso. Senha atualizada com sucesso."
+        except Exception as e:
+            return f"Erro. Falha ao atualizar senha do usuário: {e}"
+
+    def delet_user(self, email):
+        try:
+            self.cursor.execute(
+                """DELETE FROM usuarios WHERE email = %s""", (email,))
+            self.connection.commit()
+            return "Sucesso. Usuário deletado com sucesso."
+        except Exception as e:
+            return f"Erro. Falha ao deletar usuário: {e}"
