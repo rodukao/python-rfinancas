24e5cbabd584f5d8bd6d756f65828a85778d1938
rodukao <rodukao@gmail.com>
2026-04-29 14:31:00 -0300

    correcao: adicionado janela de contexto para operar banco

--- backend/Usuarios.py
+++ backend/Usuarios.py
@@ -8,81 +8,101 @@ import json
 class Usuarios:
     def __init__(self):
         self.connection = psycopg2.connect(DATABASE_URL)
-        self.cursor = self.connection.cursor()
         self.create_user_tables()
 
     def create_user_tables(self):
-        query = """CREATE TABLE IF NOT EXISTS usuarios (
+        query_table_user = """CREATE TABLE IF NOT EXISTS usuarios (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
         nome TEXT NOT NULL,
         email TEXT NOT NULL UNIQUE,
         senha TEXT NOT NULL,
         createdAt DATE
         )"""
-        self.cursor.execute(query)
-        self.connection.commit()
+
+        query_table_sessions = """CREATE TABLE IF NOT EXISTS sessoes (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        token TEXT NOT NULL,
+        email_usuario TEXT NOT NULL REFERENCES usuarios(email),
+        createdAt DATE NOT NULL
+        )"""
+
+        with self.connection:
+            cursor = self.connection.cursor()
+            cursor.execute(query_table_user)
+            cursor.execute(query_table_sessions)
+            self.connection.commit()
 
     def insert_user(self, nome, email, senha):
         salt = bcrypt.gensalt()
         senha_hashed = bcrypt.hashpw(senha.encode("utf-8"), salt)
         parametros = (nome, email, senha_hashed.decode(
             "utf-8"), datetime.now())
-        try:
-            self.cursor.execute(
-                f"""INSERT INTO usuarios (nome, email, senha, createdAt) VALUES (%s, %s, %s, %s);""", parametros)
-            self.connection.commit()
-            return "Sucesso. Usuário inserido com sucesso."
-        except Exception as e:
-            return f"Erro. Falha ao inserir usuário: {e}"
+        with self.connection:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(
+                    f"""INSERT INTO usuarios (nome, email, senha, createdAt) VALUES (%s, %s, %s, %s);""", parametros)
+                self.connection.commit()
+                return "Sucesso. Usuário inserido com sucesso."
+            except Exception as e:
+                return f"Erro. Falha ao inserir usuário: {e}"
 
     def return_user_by_email(self, email):
-        try:
-            self.cursor.execute(
-                """SELECT * FROM usuarios WHERE email = %s;""", (email,))
-            resposta = self.cursor.fetchone()
-            if resposta:
-                return {
-                    "id": resposta[0],
-                    "nome": resposta[1],
-                    "email": resposta[2],
-                    "senha": resposta[3]
-                }
-            else:
-                return None
-        except Exception as e:
-            return f"Erro. Fala ao buscar usuário: {e}"
+        with self.connection:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(
+                    """SELECT * FROM usuarios WHERE email = %s;""", (email,))
+                resposta = cursor.fetchone()
+                if resposta:
+                    return {
+                        "id": resposta[0],
+                        "nome": resposta[1],
+                        "email": resposta[2],
+                        "senha": resposta[3]
+                    }
+                else:
+                    return None
+            except Exception as e:
+                return f"Erro. Fala ao buscar usuário: {e}"
 
     def validate_user(self, email, senha):
-        try:
-            self.cursor.execute(
-                """SELECT * FROM usuarios WHERE email = %s""", (email,))
-            usuario = self.cursor.fetchone()
-            senha_banco = usuario[3].encode("utf-8")
-            valid_user = bcrypt.checkpw(senha.encode("utf-8"), senha_banco)
-            if valid_user:
-                return usuario
-            else:
-                return f"Erro. Email ou senha incorretos."
-        except Exception as e:
-            return f"Erro. Falha ao validar usuário: {e}"
+        with self.connection:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(
+                    """SELECT * FROM usuarios WHERE email = %s""", (email,))
+                usuario = cursor.fetchone()
+                senha_banco = usuario[3].encode("utf-8")
+                valid_user = bcrypt.checkpw(senha.encode("utf-8"), senha_banco)
+                if valid_user:
+                    return usuario
+                else:
+                    return f"Erro. Email ou senha incorretos."
+            except Exception as e:
+                return f"Erro. Falha ao validar usuário: {e}"
 
     def atualiza_senha(self, email, nova_senha):
         salt = bcrypt.gensalt()
         nova_senha_hashed = bcrypt.hashpw(nova_senha, salt)
         parametros = (nova_senha_hashed, email)
-        try:
-            self.cursor.execute(
-                """UPDATE usuarios SET senha = %s WHERE email = %s""", (parametros))
-            self.connection.commit()
-            return "Sucesso. Senha atualizada com sucesso."
-        except Exception as e:
-            return f"Erro. Falha ao atualizar senha do usuário: {e}"
+        with self.connection:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(
+                    """UPDATE usuarios SET senha = %s WHERE email = %s""", (parametros))
+                self.connection.commit()
+                return "Sucesso. Senha atualizada com sucesso."
+            except Exception as e:
+                return f"Erro. Falha ao atualizar senha do usuário: {e}"
 
     def delet_user(self, email):
-        try:
-            self.cursor.execute(
-                """DELETE FROM usuarios WHERE email = %s""", (email,))
-            self.connection.commit()
-            return "Sucesso. Usuário deletado com sucesso."
-        except Exception as e:
-            return f"Erro. Falha ao deletar usuário: {e}"
+        with self.connection:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(
+                    """DELETE FROM usuarios WHERE email = %s""", (email,))
+                self.connection.commit()
+                return "Sucesso. Usuário deletado com sucesso."
+            except Exception as e:
+                return f"Erro. Falha ao deletar usuário: {e}"
