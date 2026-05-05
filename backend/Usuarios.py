5e56585b82bc5a0911967c5ec460205d22b184f0
rodukao <rodukao@gmail.com>
2026-05-05 10:19:27 -0300

    feat: adicionado função que add sessões no banco

--- backend/Usuarios.py
+++ backend/Usuarios.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import psycopg2
 import bcrypt
+import secrets
 from config import DATABASE_URL
-import json
 
 
 class Usuarios:
@@ -11,6 +11,12 @@ class Usuarios:
         self.create_user_tables()
 
     def create_user_tables(self):
+        """
+        Criamos as tabelas de usuário e sessões
+        as sessões possuem um token que será vinculado ao usuário e ao navegador.
+        Ao fazer login compararemos os tokens e se for igual concederemos acesso.
+        """
+
         query_table_user = """CREATE TABLE IF NOT EXISTS usuarios (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
         nome TEXT NOT NULL,
@@ -33,6 +39,11 @@ class Usuarios:
             self.connection.commit()
 
     def insert_user(self, nome, email, senha):
+        """
+        Inserimos o usuário com nome, email, senha e a data da criação.
+        Em caso de sucesso retornamos uma string iniciada com 'Sucesso', caso contrário uma string iniciada com 'Erro'
+        """
+
         salt = bcrypt.gensalt()
         senha_hashed = bcrypt.hashpw(senha.encode("utf-8"), salt)
         parametros = (nome, email, senha_hashed.decode(
@@ -48,6 +59,12 @@ class Usuarios:
                 return f"Erro. Falha ao inserir usuário: {e}"
 
     def return_user_by_email(self, email):
+        """
+        Procuramos o usuário no banco com o email.
+        Se obtivermos sucesso ao buscar retornamos um objeto com 'id', 'nome', 'email', 'senha'.
+        Em caso de erro uma string iniciada com 'Erro'
+        """
+
         with self.connection:
             try:
                 cursor = self.connection.cursor()
@@ -67,6 +84,12 @@ class Usuarios:
                 return f"Erro. Fala ao buscar usuário: {e}"
 
     def validate_user(self, email, senha):
+        """
+        Buscamos o usuário por email e comparamos a senha retornada do banco com a senha informada criptografada.
+        Se forem as mesmas a gente retorna o usuário.
+        Se não coincidirem a gente retorna uma string iniciada com 'Erro'
+        """
+
         with self.connection:
             try:
                 cursor = self.connection.cursor()
@@ -83,6 +106,10 @@ class Usuarios:
                 return f"Erro. Falha ao validar usuário: {e}"
 
     def atualiza_senha(self, email, nova_senha):
+        """
+        Criptografamos a senha enviada e a salvamos no banco utilizando o email do usuário
+        """
+
         salt = bcrypt.gensalt()
         nova_senha_hashed = bcrypt.hashpw(nova_senha, salt)
         parametros = (nova_senha_hashed, email)
@@ -97,6 +124,10 @@ class Usuarios:
                 return f"Erro. Falha ao atualizar senha do usuário: {e}"
 
     def delet_user(self, email):
+        """
+        Deletemos usuário do banco utilizando o email
+        """
+
         with self.connection:
             try:
                 cursor = self.connection.cursor()
@@ -106,3 +137,26 @@ class Usuarios:
                 return "Sucesso. Usuário deletado com sucesso."
             except Exception as e:
                 return f"Erro. Falha ao deletar usuário: {e}"
+
+    def create_token(self):
+        """
+        Criamos e retornamos um token usando a biblioteca secrets
+        """
+
+        return secrets.token_urlsafe()
+
+    def create_session(self, email_usuario, token):
+        """
+        Criamos uma sessão no banco de dados e a atribuimos a um usuário
+        """
+
+        parametros = (token, email_usuario, datetime.now())
+        with self.connection:
+            try:
+                cursor = self.connection.cursor()
+                cursor.execute(
+                    """INSERT INTO sessoes (token, email_usuario, createdAt) VALUES (%s, %s, %s)""", parametros)
+                self.connection.commit()
+                return {"status": "success", "message": "Sessão inserida com sucesso!"}
+            except Exception as e:
+                return {"status": "error", "message": f"Falha ao inserir sessão: {e}"}
--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, make_response
 from Usuarios import Usuarios
 
 app = Flask(__name__)
@@ -24,3 +24,20 @@ def dados_usuario():
     email = request.form.get("email")
     resposta = gerencia_usuarios.return_user_by_email(email)
     return resposta
+
+
+@app.route("/insere_sessao", methods=['POST'])
+def insert_session():
+    token = gerencia_usuarios.create_token()
+    email_usuario = request.form.get("email")
+    resposta = gerencia_usuarios.create_session(email_usuario, token)
+    print(resposta)
+    if resposta['status'] == "success":
+        resp = make_response(resposta['message'])
+        resp.set_cookie("token", token,
+                        httponly=True,
+                        secure=True,
+                        samesite='Lax')
+        return resp
+    else:
+        return make_response(resposta['message']), 400
